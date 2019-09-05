484845665c32aa03ffe095c5794b48634f5c31ec
“Maxaplays” <rgmateo@hotmail.com>
2019-09-05 15:39:46 -0500

    CLOUDTAXIS-264 Authentication & route protection in angular

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,63 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {catchError} from 'rxjs/operators';
+import {throwError} from 'rxjs';
+
+export interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+}
+
+@Injectable({providedIn: 'root'})
+
+export class AuthService {
+ constructor( private http: HttpClient) {}
+
+ signup( email: string, password: string) {
+   return this.http.post<AuthResponseData>(
+     'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key= AIzaSyBKsxIOvxjTJSTYKdwdY2W_k-2gPAoXuno ',
+     {
+       email,
+       password,
+       returnSecureToken: true
+     }
+   ).pipe(
+     catchError(this.handleError)
+   );
+ }
+
+ login( email: string, password: string) {
+   return this.http.post<AuthResponseData>(
+     'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key= AIzaSyBKsxIOvxjTJSTYKdwdY2W_k-2gPAoXuno',
+     {
+       email,
+       password,
+       returnSecureToken: true
+     }
+   ).pipe(
+     catchError(this.handleError)
+   );
+ }
+
+ private handleError(errorRes: HttpErrorResponse) {
+   let errorMessage = 'An unknown error has occurred';
+   if (!errorRes.error || !errorRes.error.error) {
+     return throwError(errorMessage);
+   }
+   switch (errorRes.error.error.message) {
+     case 'EMAIL_EXISTS':
+       errorMessage = 'This email exists already';
+       break;
+     case 'EMAIL_NOT_FOUND':
+       errorMessage = 'This email was not found';
+       break;
+     case 'INVALID_PASSWORD':
+       errorMessage = 'Invalid password';
+       break;
+   }
+   return throwError(errorMessage);
+ }
+}
